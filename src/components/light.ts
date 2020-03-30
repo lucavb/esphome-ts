da09d9936e2ed34d7de1890ec015ce8451cbb3aa
Luca Becker <luca.becker@me.com>
2020-03-30 20:10:33 +0200

    updated brightness methods, version bump
    they now allow for controlling whether you like a scale from 0-100 or the esp scale from 0-255. also some min, max calls ensure that the values stay in range.

--- src/components/light.ts
+++ src/components/light.ts
@@ -8,12 +8,23 @@ export class Light extends OnOffComponent<LightState> {
         return ApiTypes.LIGHT;
     }
 
-    public getBrightness(): number | undefined {
-        return this.state.getValue()?.brightness;
+    public getBrightness(hundredScale: boolean = true): number | undefined {
+        if (this.state.getValue()?.brightness === undefined) {
+            return undefined;
+        } else {
+            const brightness = hundredScale ?
+                ((this.state.getValue()?.brightness ?? 0) / 255) * 100 :
+                this.state.getValue()?.brightness ?? 0;
+            return Math.floor(brightness);
+        }
     }
 
-    public setBrightness(brightness: number): void {
-        this.device.turnOn(this.apiType, this.id, {brightness});
+    public setBrightness(brightness: number, hundredScale: boolean = true): void {
+        let bright = hundredScale ?
+            (Math.min(Math.max(0, brightness), 100) / 100) * 255 :
+            Math.min(Math.max(0, brightness), 255);
+        bright = Math.floor(bright);
+        this.device.turnOn(this.apiType, this.id, {brightness: bright}).subscribe();
     }
 
 }
