2b797d744e88d57e225fb4ee9f2e874897a02dc7
Luca Becker <luca.becker@me.com>
2020-04-05 21:20:16 +0200

    added test for binarySensorComponent

---
+++ test/components/binarySensor.spec.ts
@@ -0,0 +1,40 @@
+import {BinarySensorComponent, BinarySensorStateEvent, LightComponent} from '../../src';
+import {CommandInterface} from '../../src/components/commandInterface';
+import {Subject} from 'rxjs';
+import {ListEntitiesBinarySensorResponse} from '../../src/api/protobuf/api';
+import {emptyCommandInterface} from '../../src/api/helpers';
+
+describe('LightComponent', () => {
+
+    let component: BinarySensorComponent;
+    const commandInterface: CommandInterface = emptyCommandInterface;
+    let stateObservable: Subject<BinarySensorStateEvent>;
+    let listEntity: ListEntitiesBinarySensorResponse;
+
+    beforeEach(() => {
+        stateObservable = new Subject<BinarySensorStateEvent>();
+        listEntity = {
+            deviceClass: 'motion',
+            key: 902392,
+            name: 'name',
+            objectId: 'objectId',
+            uniqueId: 'unique',
+            isStatusBinarySensor: false,
+        };
+        component = new BinarySensorComponent(listEntity, stateObservable, commandInterface);
+        stateObservable.next({key: listEntity.key});
+    });
+
+    it('should work with a state that has no state key', () => {
+        stateObservable.next({key: listEntity.key});
+        const status = component.status;
+        expect(status).toBe(false);
+    });
+
+    it('should work with a state that has a state key', () => {
+        stateObservable.next({key: listEntity.key, state: true});
+        const status = component.status;
+        expect(status).toBe(true);
+    });
+
+});
