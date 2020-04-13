a28982d602f5d14ccd82a787117bdefb6ab1e3ab
Luca Becker <luca.becker@sunbury.xyz>
2020-04-13 10:42:35 +0200

    wrote a test for helpers.ts in the component

---
+++ test/components/helpers.spec.ts
@@ -0,0 +1,36 @@
+import {convertNumbers} from '../../src/components/helpers';
+
+describe('esphome-ts helpers', () => {
+
+    describe('convert-numbers', () => {
+
+        it('should convert towards the esp scale', () => {
+            const input = 5;
+            const max = 100;
+            const result = convertNumbers(input, max);
+            expect(result).toBe(input / max);
+        });
+
+        it('should convert from the esp scale', () => {
+            const input = 0.8;
+            const max = 255;
+            const result = convertNumbers(input, max, true);
+            expect(result).toBe(input * max);
+        });
+
+        it('should do max conversion', () => {
+            const input = -50;
+            const max = 100;
+            const result = convertNumbers(input, max, false);
+            expect(result).toBe(0);
+        });
+
+        it('should do min conversion', () => {
+            const input = 150;
+            const max = 100;
+            const result = convertNumbers(input, max, false);
+            expect(result).toBe(1);
+        });
+
+    });
+});
