3f2b6195d4e29107872d1255c8e093cdf34512ee
Luca Becker <luca.becker@me.com>
2020-05-01 20:48:35 +0200

    switch tests written

--- test/components/switch.spec.ts
+++ test/components/switch.spec.ts
@@ -1,18 +1,50 @@
-import {SwitchComponent} from '../../src';
+import {SwitchComponent, SwitchStateEvent} from '../../src';
 import {ListEntity} from '../../src/components/entities';
 import {Subject} from 'rxjs';
-import {SwitchStateResponse} from '../../src/api/protobuf/api';
-import {emptyCommandInterface} from '../../src/api/helpers';
+import {DebugConnection} from '../testHelpers/debugConnection';
+import {MessageTypes} from '../../src/api/requestResponseMatching';
 
 
 describe('SwitchComponent', () => {
 
     let component: SwitchComponent;
     let listEntity: ListEntity;
-    let state: Subject<SwitchStateResponse>;
+    const state: Subject<SwitchStateEvent> = new Subject<SwitchStateEvent>();
+    const debugConnection: DebugConnection = new DebugConnection();
 
     beforeEach(() => {
-        component = new SwitchComponent(listEntity, state, emptyCommandInterface);
+        listEntity = {
+            key: 133,
+            name: 'my switch',
+            objectId: 'objectId',
+            uniqueId: 'uniqueId',
+        };
+        component = new SwitchComponent(listEntity, state, debugConnection);
+    });
+
+    afterEach(() => {
+        debugConnection.calls = [];
+    });
+
+    it('turnon/turnoff', () => {
+        component.turnOn();
+        expect(debugConnection.calls.length).toBe(1);
+        expect(debugConnection.calls[0][0]).toBe(MessageTypes.SwitchCommandRequest);
+        debugConnection.calls = [];
+        component.turnOff();
+        expect(debugConnection.calls.length).toBe(1);
+        expect(debugConnection.calls[0][0]).toBe(MessageTypes.SwitchCommandRequest);
+    });
+
+    it('returns the state', () => {
+        state.next({key: listEntity.key});
+        expect(component.status).toBe(false);
+        state.next({key: listEntity.key, state: true});
+        expect(component.status).toBe(true);
+    });
+
+    it('says that it is a switch', () => {
+        expect(component.type).toBe('switch');
     });
 
 });
---
+++ test/testHelpers/debugConnection.ts
@@ -0,0 +1,13 @@
+import {CommandInterface} from '../../src/components/commandInterface';
+import {MessageTypes} from '../../src/api/requestResponseMatching';
+
+
+export class DebugConnection implements CommandInterface {
+
+    public calls: [MessageTypes, Uint8Array][] = [];
+
+    send(type: MessageTypes, data: Uint8Array): void {
+        this.calls.push([type, data]);
+    }
+
+}
